7f340c6a76a5bae508fb602feb964c3b88abe6af
Nereklamiok <kosarev_kirilll@mail.ru>
2017-12-10 15:56:07 +0300

    Доделал основу

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function initMap() {
+    var baloon = {lat:59.938808, lng:30.323250};
+    var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 18,
+    center: {lat:59.9391009,lng:30.3239469},
+    disableDefaultUI: true
+    });
+    var marker = new google.maps.Marker({
+    position:baloon,
+    icon:'../img/baloon.png',
+    map: map
+    });
+}
